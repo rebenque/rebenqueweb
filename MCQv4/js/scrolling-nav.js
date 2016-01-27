51c41a873453796474205d941aa48caec35af437
Javier de Bona <Piperita@Javiers-iMac.local>
2016-01-27 15:31:03 +0100

    “”

--- MCQv4/js/scrolling-nav.js
+++ MCQv4/js/scrolling-nav.js
@@ -1,10 +1,33 @@
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
+    $('a.page-scroll-parallax').bind('click', function(event) {
+        var $anchor = $(this);
+        //$('html, body, .rbq-parallax').stop().animate({
+        $('.rbq-parallax').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top + $('.rbq-parallax').scrollTop()
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+$(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body, .rbq-parallax').stop().animate({
+        //$('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
 });
+
+
+/*
+function setBindings() {
+  $("li a").click(function(e){
+    e.preventDefault();
+    var sectionId = e.currentTarget.id + "Section";
+    $("html").animate({
+      scrollTop: $("#" + sectionId).position().top +"px"
+    },1000)
+  });
+  */
